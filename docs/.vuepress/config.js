d9aef9d60b4381959a2f47fc63b34cb088c4ecac
Michael Nien <mobile.michael.nien@gmail.com>
2025-07-26 23:44:08 +0800

    新增 vuepress 設定元件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'FintechNews',
+  description: 'FintechNews',
+  base: '/FintechNews/',
+  repo: 'https://github.com/MichaelNien/FintechNews',
+  plugins: [
+	'vuepress-plugin-mermaidjs'
+  ],
+  themeConfig: {
+    sidebarDepth: 0,
+    sidebar: [
+      ['/', '首頁'],
+    ],
+    nav: [
+    ]
+  }
+}

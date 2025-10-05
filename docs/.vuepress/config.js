62b0917e5c54d15d10468f1dac6e25f4069f62c2
Michael Nien <mobile.michael.nien@gmail.com>
2025-10-06 00:04:14 +0800

    新增 2025 41週

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
         collapsable: false,
         sidebarDepth: 1,
         children: [
+			['/2025/2025_41.md', '41'],
 			['/2025/2025_40.md', '40'],
 			['/2025/2025_39.md', '39'],
 			['/2025/2025_38.md', '38'],

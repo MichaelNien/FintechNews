bc891dc2e5caa0d9e17852c4254fdd7eea112cc0
Michael Nien <mobile.michael.nien@gmail.com>
2026-06-30 22:48:55 +0800

    新增 2026 27週

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
         collapsable: false,
         sidebarDepth: 1,
         children: [
+			['/2026/2026_27.md', '27'],
 			['/2026/2026_26.md', '26'],
 			['/2026/2026_25.md', '25'],
 			['/2026/2026_24.md', '24'],
